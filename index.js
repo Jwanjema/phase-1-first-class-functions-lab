c18df8fb83823f11a36b3fa1f4a0f56e33ec6d68
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-20 18:11:40 +0300

    add index.js .results.json

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+returnLastTwoDrivers(drivers);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+createFareMultiplier()
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, selectedDrivers) {
+    return selectedDrivers(arrayOfDrivers)
+}
+
